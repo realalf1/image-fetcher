3514686e66f0aa3b423f71a2e4057bf342cb92c3
Alfiyan <152812058+realalf1@users.noreply.github.com>
2024-01-05 18:08:49 +0700

    Create img_fetcher.py

---
+++ img_fetcher.py
@@ -0,0 +1,33 @@
+import requests
+
+class Images():
+    def __init__(self, source, destination):
+        self.name = "Fetch Images with Python"
+        self.source = source
+        self.destination = destination
+        self.status = False
+
+    def fetch(self):
+        r = requests.get(self.source)
+
+        if r.status_code == 200:
+            with open(self.destination, "wb") as f:
+                f.write(r.content)
+                print("\nFetching image successfully")
+            self.status = True
+        
+        else:
+            print(f"Failed to fetch {self.source}")
+            
+    def run(self):
+        print(f"Source     : {self.source}")
+        print(f"Destination: {self.destination}")
+        print(f"Status     : {self.status}")
+        self.fetch()
+        print(f"Status     : {self.status}")
+
+if __name__ == "__main__":
+    src = "https://salam.uinsgd.ac.id/dashboard/assets/logo_salam.png"
+    dst = f"./img/logo.png"
+    image = Images(src, dst)
+    image.run()
